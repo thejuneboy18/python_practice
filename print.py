ade8fdedfae69d2943afc0c858ed6e8b0667155e
thejuneboy18 <pratikpatil1806@gmail.com>
2024-08-14 13:30:55 +0530

    14 aug

---
+++ print.py
@@ -0,0 +1,15 @@
+# print("pratik patil")
+
+# print("iphone", "samsung", "nothing1")
+
+# print("pratik", end=" ")
+# print("patil")
+
+# print("pratik", end=" ")
+# print("patil", end=" ")
+# print("eknath")
+
+# print("iphone", "samsung", "nothing1", sep=", ")
+
+
+                                    
